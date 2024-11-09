c742c93767b6650e4b06e5e10cd5812ba6d3949c
atmabdullaham <atmabdullaham@gmail.com>
2024-11-10 03:51:04 +0600

    practice the array length and array index

---
+++ index.js
@@ -0,0 +1,14 @@
+const numbers = [23, 32, 54, 45, 64, 34, 65];
+
+// Index will be started at 0;
+
+console.log(numbers)
+console.log(numbers[1])
+
+
+const fourth = numbers[3];
+console.log(fourth);
+
+
+numbers[0] = 54;
+console.log(numbers);
---
+++ length.js
@@ -0,0 +1,2 @@
+const numbers = [1, 2, 5, 64, 54, 5, 3, 5, 5, 6];
+console.log(numbers.length);
