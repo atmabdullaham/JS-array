d5f1624f18bcb953ebb569e1d33bc8d9386dfdad
atmabdullaham <atmabdullaham@gmail.com>
2024-11-10 01:19:40 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,15 @@
+const num1 = 12;
+const num2 = 14;
+const num3 = 25;
+const num4 = 245;
+
+// array declare
+
+const numbers = [12, 22, 32, 24, 14, 20];
+
+// array and the main things of the numbers
+
+let friends = ['Abdul', 'Babul', 'Sakib'];
+const passed = [true, false, true, false];
+
+const mixedArray = [12, 'Alahbad', true, 45.00];
